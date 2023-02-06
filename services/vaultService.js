873953f11521102e5b0d43a3de68832c3123ff5a
Sandor Szabolcs <szabolcs.sandor@prodigy-it-solutions.com>
2023-02-06 18:21:48 +0200

    Modify how role and secret is passed to Vault

--- services/vaultService.js
+++ services/vaultService.js
@@ -1,22 +1,36 @@
+const fs = require('fs');
+
 const getEncryptionKey = () => 
 {
     const vault = require("node-vault")({
         apiVersion: "v1",
-        endpoint: process.env.VAULT_ADDRESS,
+        endpoint: "http://127.0.0.1:8200",
     });
-     
-    const roleId = process.env.ROLE_ID;
-    const secretId = process.env.SECRET_ID;
+    
+    let role_id;
+    fs.readFile('role_id', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        role_id = data;
+    });
 
-    return vault.approleLogin({
-        role_id: roleId,
-        secret_id: secretId,
-    })
-        .then(result => vault.token = result.auth.client_token)
-        .then(() =>
-            vault.read("secret/data/ibl")
-                .then(result => result.data.encryptionkey ))
-                .catch(err => console.error("/data",err));
+    // Read the role ID and secret ID from the secret backend
+    return vault.write("auth/approle/role/dev-role/secret-id")
+        .then(secret => {
+            return vault.approleLogin({
+                role_id: role_id,
+                secret_id: secret.data.secret_id,
+            })
+                .then(result => {
+                    vault.token = result.auth.client_token;
+        
+                    return vault.read("secret/data/ibl")
+                        .then(result => result.data.encryptionkey)
+                        .catch(err => console.error("/data",err));
+                });
+            });
 };
 
 module.exports = {
